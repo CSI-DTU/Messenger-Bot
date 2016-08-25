fa946b2700edcc9d675c046ecdad5fac4ad51434
Nikhil Kumar Singh <nikhilksingh97@gmail.com>
2016-08-25 07:45:57 -0700

    Added a flask app

--- csi_bot.py
+++ csi_bot.py
@@ -1,21 +1,89 @@
+import sys
 import aiml
-import telepot
-import time
-
-def handle(msg):
-    chat_id = msg['chat']['id']
-    command = msg['text']
-    username = msg['from']['first_name']
-    print 'Got command: %s' % command
-    tbot.sendMessage(chat_id,bot.respond(command))
+import json
+import requests
+from flask import Flask, request
+app = Flask(__name__)
 
 bot = aiml.Kernel()
 bot.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B")
+bot.setBotPredicate("botmaster","CSI")
+bot.setBotPredicate("name","CSI-BOT")
+
+def botresponse(query):
+    return bot.respond(query)
+               
+
+@app.route('/', methods=['GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == 'hello':
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
 
 
+@app.route('/', methods=['POST'])
+def webook():
+    data = request.get_json()
+    log(data)  
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+                    send_message(sender_id, botresponse(message_text))
+            
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": 'XXXXXXXXXXXXXXXXXXXXXX'
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
-tbot = telepot.Bot('221746913:AAGDtUMCEW0eLNJqjHC8YhEthz_PCCrfTnk')
-tbot.message_loop(handle)
-print 'I am listening ...'
-while 1:
-    time.sleep(10)
+    
+    
